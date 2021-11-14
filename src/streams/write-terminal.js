ec739757c3ad8c8b57b910e03f9296544d22a13c
Aleksey Kouzmenko <iskeyiv@gmail.com>
2021-11-14 23:14:57 +0400

    fix: move cursor to the next line after write to stdout

--- src/streams/write-terminal.js
+++ src/streams/write-terminal.js
@@ -2,7 +2,7 @@ import {Writable} from "stream";
 
 export class WriteTerminal extends Writable {
     _write(chunk, encoding, callback) {
-        process.stdout.write(chunk);
+        process.stdout.write(chunk + '\n');
         callback();
     }
 }
