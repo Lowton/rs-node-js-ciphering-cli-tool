17d1255f84dbb29ddf65aefafc2ddd5a6e1ceece
Aleksey Kouzmenko <iskeyiv@gmail.com>
2021-11-21 23:09:50 +0400

    refactor: adapting cli-tool for testing

--- src/cli-tool.js
+++ src/cli-tool.js
@@ -1,25 +1,29 @@
-import {ArgumentParser as ArgumentsParser} from "./argument-parser.js";
 import {pipeline} from "node:stream";
+import {ArgumentParser as ArgumentsParser} from "./argument-parser.js";
 
-function handleError(description, code) {
-    process.stderr.write(description);
-    process.exit(code);
+export class CliTool {
+	constructor(arguments_) {
+		this.args = arguments_;
+	}
+
+	start() {
+		const properties = new ArgumentsParser(this.args.slice(2)).parse();
+		pipeline(properties.input,
+			...properties.config,
+			properties.output,
+			error => {
+				switch (error?.type) {
+				case "ConfigError": this.handleError(error?.description, 1001); break;
+				case "WriteFileError": this.handleError(error?.description, 1002); break;
+				case "ReadFileError": this.handleError(error?.description, 1003); break;
+				case "ArgumentError": this.handleError(error?.description, 1004); break;
+				}
+			}
+		);
+	}
+
+	handleError(description, code) {
+		process.stderr.write(description);
+		process.exit(code);
+	}
 }
-
-function main() {
-    const properties = new ArgumentsParser(process.argv.slice(2)).parse();
-    pipeline(properties.input,
-        ...properties.config,
-        properties.output,
-        error => {
-            switch (error?.type) {
-                case "ConfigError": handleError(error?.description, 1001); break;
-                case "WriteFileError": handleError(error?.description, 1002); break;
-                case "ReadFileError": handleError(error?.description, 1003); break;
-                case "ArgumentError": handleError(error?.description, 1004); break;
-            }
-        }
-    );
-}
-
-main();
